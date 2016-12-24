6335ac41e18a0dd291fdd92584fec6bef1e7b834
Casey <soup-bowl@users.noreply.github.com>
2016-12-23 19:02:48 -0500

    Further email support development

--- app/Controllers/MailController.php
+++ app/Controllers/MailController.php
@@ -3,6 +3,7 @@
 use MailTwelve\Services\settingService;
 use Herbert\Framework\Models\Post;
 use MailTwelve\Helper;
+use Zend\Mail\Storage\Imap;
 
 class MailController {
 	protected $SettingsService;
@@ -16,16 +17,71 @@ class MailController {
 			return view('@MailTwelve/error/noimap.twig');
 		}
 		
+		// Load up configuration files and negate IP addresses for faster comms.
 		$userConfig = $this->SettingService->GetUserSettings();
 		$sysConfig  = $this->SettingService->GetMailSystemSettings();
 		$mailips = ['incoming' => gethostbyname($sysConfig['incomingUrl']), 'outgoing' => gethostbyname($sysConfig['outgoingUrl'])];
 		
-		$mbox = \imap_open("{".$mailips['incoming'].":".$sysConfig['incomingPort']."}INBOX", $userConfig['username'], $userConfig['password']);
-		die(var_dump($mbox));
+		/*
+		// Connect to mailbox, or show an error page if the connection fails.
+		$mbox = null;
+		$mbox = \imap_open("{".$mailips['incoming'].":".$sysConfig['incomingPort']."/imap/readonly/notls}INBOX", $userConfig['username'], $userConfig['password']);
+		if (!$mbox) {
+			return view('@MailTwelve/error/imapExtError.twig', [ 'error' => imap_last_error() ]);
+		}
+		
+		$headers = imap_headers($mbox);
+		$head1 = [];
+		foreach ($headers as $header) {
+			array_push($head1, imap_rfc822_parse_headers($header));
+		}
+		//die(var_dump($headers));
+		
+		imap_close($mbox);*/
+		
+		$mailService = new Imap([
+			'host'     => $mailips['incoming'],
+			'port'     => $sysConfig['incomingPort'],
+			'user'     => $userConfig['username'],
+			'password' => $userConfig['password'],
+		]);
+		$mailService->selectFolder('INBOX');
+		
+		$mail = new \stdClass();
+		$mail->count   = $mailService->countMessages(); 
+		$mail->letters = [];
+		
+		/*for ($i = 1; $i < $mail->count; $i++) {
+    		$letter;
+			try {
+				$letter = $mailService->getMessage($i);
+			} catch {
+				$letter = "Error reading message: " . $e;
+			}
+			array_push($mail->letters, $letter);
+		}*/
+		
+		//die(var_dump( $mailService->getFolders() ));
+		
+		$mailArray = [];
+		foreach ($mailService as $messageNum => $message) {
+    		$mailObj = new \stdClass();
+			$mailObj->num     = $messageNum;
+			$mailObj->title   = $message->subject;
+			$mailObj->from 	  = $message->from; 
+			$mailObj->cc      = (isset($message->cc)) ? $message->cc : null;
+			$mailObj->date    = $message->date;
+			$mailObj->content = $message->getContent();
+			
+			array_push( $mailArray, $mailObj );
+		}
+		
+		//die(print_r( $mailArray ));
 		
 		return view('@MailTwelve/mail/home.twig', [
-			'address' => $userConfig['username'],
-			'title'   => Helper::get('pluginName')
+			'address'    => $userConfig['username'],
+			'title'      => Helper::get('pluginName'),
+			'collection' => $mailArray
 		]);
 	}
 }
